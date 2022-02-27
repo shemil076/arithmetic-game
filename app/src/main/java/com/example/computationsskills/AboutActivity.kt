278938d4bbaae5048f49caa9475cddc7cd9bd8de
Pramuditha Karunarathna <shemil076@gmail.com>
2022-02-27 10:11:56 +0530

    :seedling: Initialized the about activity

--- app/src/main/java/com/example/computationsskills/AboutActivity.kt
+++ app/src/main/java/com/example/computationsskills/AboutActivity.kt
@@ -3,10 +3,9 @@ package com.example.computationsskills
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        setContentView(R.layout.activity_about)
     }
 }
