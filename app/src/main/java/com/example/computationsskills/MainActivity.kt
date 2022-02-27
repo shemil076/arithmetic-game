64061faa64e2802ecd40ba6a2dc1e82077ae3b68
Pramuditha Karunarathna <shemil076@gmail.com>
2022-02-27 18:12:21 +0530

    :seedling: initialized the about activity

--- app/src/main/java/com/example/computationsskills/MainActivity.kt
+++ app/src/main/java/com/example/computationsskills/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.computationsskills
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        var btnNewGame = findViewById<Button>(R.id.newGame)
+        var btnAbout = findViewById<Button>(R.id.about)
+
+
+        btnAbout.setOnClickListener {
+            val aboutActivityIntent = Intent(this, AboutActivity::class.java)
+            startActivity(aboutActivityIntent)
+        }
+
     }
 }
