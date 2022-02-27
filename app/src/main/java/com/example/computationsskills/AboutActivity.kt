e5035d2af2f050c41f7055f451949d556d4bb759
Pramuditha Karunarathna <shemil076@gmail.com>
2022-02-27 20:26:17 +0530

    :children_crossing: partially completed the about activity

--- app/src/main/java/com/example/computationsskills/AboutActivity.kt
+++ app/src/main/java/com/example/computationsskills/AboutActivity.kt
@@ -1,11 +1,23 @@
 package com.example.computationsskills
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class AboutActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        var txtUowNumber = findViewById<TextView>(R.id.txtUowNumber)
+        var txtIITNumber = findViewById<TextView>(R.id.txtIITNumber)
+        var txtName = findViewById<TextView>(R.id.txtName)
+
+        txtUowNumber.text = "1836048"
+        txtIITNumber.text = "20200612"
+        txtName. text = "Pramuditha Karunarathna"
+
     }
 }
