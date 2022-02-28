27fe1dd22fe4d0bf405ba1f070fd3fc2e37e99ed
Pramuditha Karunarathna <shemil076@gmail.com>
2022-02-28 22:05:33 +0530

    :children_crossing: done with about (popup window)

--- app/src/main/java/com/example/computationsskills/MainActivity.kt
+++ app/src/main/java/com/example/computationsskills/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.computationsskills
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.PopupWindow
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,10 +24,26 @@ class MainActivity : AppCompatActivity() {
             val gameActivityIntent = Intent(this,GameActivity::class.java)
             startActivity(gameActivityIntent)
         }
-        btnAbout.setOnClickListener {
-            val aboutActivityIntent = Intent(this, AboutActivity::class.java)
-            startActivity(aboutActivityIntent)
+//        btnAbout.setOnClickListener {
+//            val aboutActivityIntent = Intent(this, AboutActivity::class.java)
+//            startActivity(aboutActivityIntent)
+//        }
+
+        btnAbout.setOnClickListener{
+            val window = PopupWindow(this)
+            window.isFocusable
+            val view = layoutInflater.inflate(R.layout.aboutpopup,null)
+            window.contentView = view
+//            window.showAtLocation(this,)
+            window.showAtLocation(view, Gravity.CENTER,0,0)
+            view.setOnClickListener {
+                window.dismiss()
+            }
         }
+
+
+
+
 
     }
 }
